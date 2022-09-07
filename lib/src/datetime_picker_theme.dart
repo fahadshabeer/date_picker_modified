2edfed4282f4bb8de568662e691ed700b660259d
Fahad <fadiktk000@gmail.com>
2022-09-07 12:30:58 +0500

    fahad

---
+++ lib/src/datetime_picker_theme.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+// Migrate DiagnosticableMixin to Diagnosticable until
+// https://github.com/flutter/flutter/pull/51495 makes it into stable (v1.15.21)
+class DatePickerTheme with DiagnosticableTreeMixin {
+  final TextStyle cancelStyle;
+  final TextStyle doneStyle;
+  final TextStyle itemStyle;
+  final Color backgroundColor;
+  final Color doneButtonColor;
+  final TextStyle titleStyle;
+  final Color cancelButtonColor;
+  final Color? headerColor;
+
+  final double containerHeight;
+  final double titleHeight;
+  final double itemHeight;
+
+  const DatePickerTheme({
+    this.cancelStyle = const TextStyle(color: Colors.black54, fontSize: 16),
+    this.doneStyle = const TextStyle(color: Colors.blue, fontSize: 16),
+    this.itemStyle = const TextStyle(color: Color(0xFF000046), fontSize: 18),
+    this.titleStyle= const TextStyle(color: Colors.black,fontSize: 18),
+    this.backgroundColor = Colors.white,
+    this.headerColor,
+    this.cancelButtonColor=Colors.white,
+    this.doneButtonColor=Colors.white,
+    this.containerHeight = 210.0,
+    this.titleHeight = 44.0,
+    this.itemHeight = 36.0,
+  });
+}
